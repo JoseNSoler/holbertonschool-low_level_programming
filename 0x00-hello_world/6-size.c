f527250ac57700d2a04f75d984c0e6665ef58b9d
jnsoler140214 <josenico1212@gmail.com>
2020-09-18 13:08:25 -0500

    06

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 /**
-*main - prints 2
+*main - prin1s 3 all sizes char, int, long, double and float
 *
 *Return: 0
 */
@@ -7,21 +7,16 @@
 int main(void)
 {
 
+char aaa;
+int bbb;
+long ccc;
+double ddd;
+float eee;
 
-char a;
-int b;
-long c;
-double d;
-float e;
-
-printf("Size of a char: %d byte(s)\n", sizeof(a));
-printf("Size of an int: %d byte(s)\n", sizeof(b));
-printf("Size of a long int: %d byte(s)\n", sizeof(c));
-printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-printf("Size of a float: %d byte(s)\n", sizeof(e));
+printf("Size of a char: %d byte(s)\n", sizeof(aaa));
+printf("Size of an int: %d byte(s)\n", sizeof(bbb));
+printf("Size of a long int: %d byte(s)\n", sizeof(ccc));
+printf("Size of a long long int: %d byte(s)\n", sizeof(ddd));
+printf("Size of a float: %d byte(s)\n", sizeof(eee));
 return (0);
-}
-
-
-
 }
