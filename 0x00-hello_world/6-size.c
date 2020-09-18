e46733cce460077954fd88e4d54b57863bbcd1ad
jnsoler140214 <josenico1212@gmail.com>
2020-09-18 13:02:05 -0500

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+/**
+*main - prints 2
+*
+*Return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+
+
+char a;
+int b;
+long c;
+double d;
+float e;
+
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
+printf("Size of a long int: %d byte(s)\n", sizeof(c));
+printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+printf("Size of a float: %d byte(s)\n", sizeof(e));
+return (0);
+}
+
+
+
+}
