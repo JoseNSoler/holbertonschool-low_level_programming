17c191501a3b1f6f163b14d964dd2fdb8c786100
jnsoler140214 <josenico1212@gmail.com>
2020-09-18 15:00:30 -0500

    09

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - combinations 
- * Return: 0 - numbers and ,
+ * main - numbers with ,
+ * Return: 0
  *
  */
 
-int main(void) {
-        int x;
-        do {
-                for (x = 0; x <= 9; x++) {
-                        putchar(x + 48);
-                        if (x != 9) {
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        } while (x < 9);
-        putchar('\n');
-        return (0);
+int main(void)
+{
+int x;
+do {
+for (x = 0; x <= 9; x++)
+{
+putchar(x + 48);
+if (x != 9)
+{
+putchar(',');
+putchar(' ');
+}}
+} while (x < 9);
+putchar('\n');
+return (0);
 }
