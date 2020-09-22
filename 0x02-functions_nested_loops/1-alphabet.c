f941d03106d3e3427917cdda270fbb5ef41b562a
jnsoler140214 <josenico1212@gmail.com>
2020-09-22 16:54:12 -0500

    01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - alphabet with header
+ * Return: void
+ *
+ */
+
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar(\n);
+}
