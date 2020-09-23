e39406ab743e3f3990817bc03c003040ad62e1ff
jnsoler140214 <josenico1212@gmail.com>
2020-09-22 19:06:37 -0500

    07

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints last digit of a int
+ *@r: integer
+ * Return: last number
+ */
+
+int print_last_digit(int r)
+{
+
+	int x = r % 10;
+
+	if (x > 0)
+	{
+		_putchar(x + '0');
+	}
+	else
+	{
+		a = a * -1;
+		_putchar(a + '0');
+	}
+
+}
