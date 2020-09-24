d27d37faa3f49aba062f3ad3ebc729c12fb4ee1a
jnsoler140214 <josenico1212@gmail.com>
2020-09-24 16:08:38 -0500

    100

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -10,26 +10,26 @@
 int main(void)
 {
 
-int x  = 50;
+	int x  = 50;
 
-int y = 50;
+	int y = 50;
 
-while (y <= 59)
-{
-for ( ; x <= 59; x++)
-{
-putchar(y);
-putchar(x);
-if (x == 59 && y == 59)
-{
-putchar('\n');
-break;
-}
-putchar(',');
-putchar(' ');
-}
-x = 50;
-y++;
-}
-return (0);
+	while (y <= 59)
+	{
+		for ( ; x <= 59; x++)
+		{
+			putchar(y);
+			putchar(x);
+			if (x == 59 && y == 59)
+			{
+				putchar('\n');
+				break;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	x = 50;
+	y++;
+	}
+	return (0);
 }
