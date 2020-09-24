5e5eac736099cf67484bc509a38b161e53b5b7a9
jnsoler140214 <josenico1212@gmail.com>
2020-09-24 17:11:28 -0500

    0x04 02

---
+++ 0x04-more_functions_nested_loops/2-nul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two int
+ * @a: character
+ * @b: second character
+ * Return: 1 digit, 0 false
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
