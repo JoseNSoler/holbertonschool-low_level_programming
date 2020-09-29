d221048438b2e7a9912eb361975d47d51da82f46
jnsoler140214 <josenico1212@gmail.com>
2020-09-29 18:57:18 -0500

    0x05 05

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverse a string to his variable original
+ * @s: adress string to reverse and save it
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int x = 0, y = 0;
+	int z;
+	char a[10];
+
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	for (z = x - 1; x >= 0; x--)
+	{
+		a[y] = s[z];
+		y++;
+	}
+
+	a[y + 1] = '\0';
+
+	for (z = 0; z <= i - 1; z++)
+	{
+		s[z] = a[z];
+	}
+
+}
