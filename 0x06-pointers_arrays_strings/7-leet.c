4a5d6e1d29af5f3b50c74428517f281a1605e633
jnsoler140214 <josenico1212@gmail.com>
2020-10-01 23:52:22 -0500

    0x06 07

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * leet - encode in base 1337
+ * @a: array to analize
+ *
+ *
+ * Return: char encoded
+ */
+
+
+char *leet(char *a)
+{
+	int x = 0;
+	int y;
+	char aeo["aeotlAEOTL"];
+	char b["4307143071"];
+
+
+	while (a[x] != '\0')
+	{
+		for (y = 0; aeo[y != '\0'; y++])
+		{
+			if (a[y] == abc[x])
+			{
+				a[y] = b[x];
+				break;
+			}
+		}
+
+		x++
+	}
+	return (a);
+}
