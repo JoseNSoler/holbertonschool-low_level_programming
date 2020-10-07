d2e23e8631d8e533c5ad80fbe140893898c2ed7d
jnsoler140214 <josenico1212@gmail.com>
2020-10-07 16:41:30 -0500

    0x07 05

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+
+/**
+ * _strstr - locates a substring
+ * @haystack: pointer - string to analize
+ * @needle: pointer - string to compare
+ *
+ * Return: pointer to the start of the coincidence
+ */
+
+
+
+char *_strstr(char *haystack, char *needle)
+{
+	int x;
+	int y = 0;
+	int z = 0;
+
+	for (x = 0; haystack[x]; x++)
+	{
+		for (y = 0; needle[y]; y++)
+		{
+			if (haystack[x] == needle[y])
+			{
+			break;
+			}
+		}
+		for (; x < y; x++)
+		{
+			if (haystack[x] == needle[y])
+			{
+				z++;
+				y++;
+			}
+		}
+		if (y == z)
+		{
+			return (haystack + x);
+		}
+	}
+	return ('\0');
+}
