5fb0e8738b7c8c443a3bdd2e4602bb7f16f3f0e9
jnsoler140214 <josenico1212@gmail.com>
2020-10-07 15:19:54 -0500

    README and 0x00

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: array to analize
+ * @b: byte to print
+ * @n: insigned int - number of bytes apply the function
+ *
+ * Return: s - pointer to the original array
+ */
+
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	unsigned int x = 0;
+
+	while(x < n)
+	{
+		s[x] = b;
+		x++;
+	}
+	return(s);
+
+}
