e5373c811959f66f2d9a738fa701a67f8ff3e125
jnsoler140214 <josenico1212@gmail.com>
2020-10-07 21:52:06 -0500

    0x08 README

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+
+/**
+ * _puts_recursion - prints a string followed by new line
+ * @s: pointer to print
+ *
+ * Return: void function - final print
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	int x = 0;
+
+	if (s[x] != '\0')
+	{
+		putchar(s[x]);
+		x++;
+		_puts_recursion(s + x);
+	}
+	else
+	putchar('\n');
+}
---
+++ 0x08-recursion/README.md~
@@ -0,0 +1,1 @@
+This is a README file for the project 0x07 - pointers arrays and strings
