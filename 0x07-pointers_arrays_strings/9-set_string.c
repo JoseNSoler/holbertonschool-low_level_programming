85201a8d980b45822a7d1481e7e744befb62dd8f
jnsoler140214 <josenico1212@gmail.com>
2020-10-07 23:57:54 -0500

    0x07 09

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer - array to analize
+ * @to: pointer -  destiny to apply the change
+ *
+ * Return: void function - print function
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
