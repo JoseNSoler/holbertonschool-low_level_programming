e05ea50325adba24c7ded7fe07679cdcfdd3c7db
jnsoler140214 <josenico1212@gmail.com>
2020-10-11 22:19:36 -0500

    0x0A 01

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,14 +7,14 @@
  * @argc: argument count
  * @argv: arguments vector
  *
- * Description: returns the name of the file
+ * Description: returns numbers of arguments passed
  * Return: 0 in succes
  */
 
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[]__attribute__((unused)))
 {
 
-	printf("%s\n", argv[0]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
