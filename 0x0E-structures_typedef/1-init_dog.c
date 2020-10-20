3ae5f747808ee8bf90342cb9196b419de8143dc7
jnsoler140214 <josenico1212@gmail.com>
2020-10-19 21:32:37 -0500

    0x0E 01

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - Initialize doggo structure
+ * @d: doggo strcture
+ * @name: name of doggo
+ * @age: age of doggo
+ * @owner: owner of doggo
+ *
+ * Return: void function
+ **/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	d->name = name;
+	d->.age = age;
+	d->owner = owner;
+}
