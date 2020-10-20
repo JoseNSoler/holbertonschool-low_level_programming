a9d4fdfbea84a4d27e2b0318a5f8a57810c47cc9
jnsoler140214 <josenico1212@gmail.com>
2020-10-19 21:24:24 -0500

    0x0E 0 header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - Dog structure
+ *
+ * @name: Name of doggo
+ * @age: Age of dogdo
+ * @owner: Owner of the dogdo
+ **/
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+
+#endif
