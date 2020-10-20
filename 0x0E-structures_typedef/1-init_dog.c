08f26b0ef1c952988f34e26a430a67943550281b
jnsoler140214 <josenico1212@gmail.com>
2020-10-19 21:34:40 -0500

    0x0E 01

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,11 +10,12 @@
  *
  * Return: void function
  **/
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
 		return;
 	d->name = name;
-	d->.age = age;
+	d->age = age;
 	d->owner = owner;
 }
