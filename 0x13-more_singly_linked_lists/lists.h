104d3d836cfca5cfc0c7a30d712e36a7da21bb24
jnsoler140214 <josenico1212@gmail.com>
2020-11-04 22:37:42 -0500

    Fix header

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -10,18 +10,18 @@
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: points to the next node
+ * @next: pointer to next structure-element list
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Descriptio: singly linked list node structure
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
-
 size_t print_listint(const listint_t *h);
+
+
 
 
 #endif
