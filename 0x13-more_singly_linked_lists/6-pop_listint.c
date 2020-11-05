1519f9910d8ae181c0d8b3b1bd13530141af731f
jnsoler140214 <josenico1212@gmail.com>
2020-11-04 23:38:28 -0500

    Update Task 06 and header

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node-element of a linked list
+ * @head: pointer to header
+ *
+ * Return: pointer to head-list
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int tempint = 0;
+
+	tmp = *head;
+	if (temp)
+	{
+		tempint = temp->n;
+		*head = (*head)->next;
+		free(temp);
+	}
+	return (tempint);
+}
