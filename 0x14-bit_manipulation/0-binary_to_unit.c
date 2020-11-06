421f13fd7dd358dca3066a16d7bf6742ababd87c
jnsoler140214 <josenico1212@gmail.com>
2020-11-05 23:57:20 -0500

    Update Task 0, readme file and header

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,65 @@
+/**
+ * _pow_recursion - returns the power of x in base y
+ *
+ * @x: base number
+ * @y: number to elevate
+ * Return: number powered
+ */
+
+unsigned int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y > 1)
+	{
+		y = _pow_recursion(x, y - 1);
+	}
+	else
+	{
+		return (x * y);
+	}
+	return (x * y);
+}
+
+
+
+/**
+ * binary_to_uint - Convert an array of char of "1 and 0" to
+ * decimal notation
+ *
+ * @b: pointer to array to analize
+ *
+ * Return: final- number in base decimal
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+	unsigned int x = 0, y = 0, copiax = 0, final = 0;
+
+	if (b && b[x])
+	{
+		while (b[x] != '\0')
+		{
+			if (b[x] > '1' || b[x] < '0')
+			{
+				return (0);
+			}
+			x++;
+		}
+		x--;
+		copiax = x;
+		for (; y <= copiax; y++)
+		{
+			if (b[y] == '1')
+			{
+				final += _pow_recursion(2, x);
+			}
+			x--;
+		}
+		return (final);
+	}
+	return (0);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+unsigned int binary_to_uint(const char *b);
+
+
+
+
+#endif /*HOLBERTON_H_*/
