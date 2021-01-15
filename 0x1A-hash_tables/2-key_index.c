c1dc6dbaa21e632822ceae1c33795c41d426e956
JoseNSoler <josenico1212@gmail.com>
2021-01-15 20:04:31 +0000

    FIX header

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -6,8 +6,7 @@
  * @key: Key to get the index
  * @size: size of the array of the hash table
  *
- * Return: index of the key
- *
+ * Return: index of the key*
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
